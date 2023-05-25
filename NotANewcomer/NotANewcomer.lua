25b987333ff9f94c6b641b6272a22fa5a7f0eefc
Jo Li KMC <102710555+JoLiKMC@users.noreply.github.com>
2023-05-25 08:02:51 -0400

    Updating to v1.0.5
    I finally got it working how I want it!  Yay!

--- NotANewcomer/NotANewcomer.lua
+++ NotANewcomer/NotANewcomer.lua
@@ -1,19 +1,19 @@
-local frame=CreateFrame("Frame");
-local guildName = GetGuildInfo("player")
+local frame=CreateFrame("Frame")
 
-frame:RegisterEvent("CHAT_MSG_SYSTEM");
+frame:RegisterEvent("UI_INFO_MESSAGE")
 frame:SetScript("OnEvent",function(self,event,...)
 	DEFAULT_CHAT_FRAME:AddMessage("|cffffcc00Not a Newcomer!: |cffffffffI'm checking your guild status…")
 	if (IsInGuild()) then
+		local guildName=GetGuildInfo("player")
 		GetGuildInfo("player")
 		if (guildName == "Newcomers") then
 			DEFAULT_CHAT_FRAME.editBox:SetText("/gquit") ChatEdit_SendText(DEFAULT_CHAT_FRAME.editBox, 0)
 			DEFAULT_CHAT_FRAME:AddMessage("|cffffcc00Not a Newcomer!: |cffffffffLooks like you're part of the |cff00FF00Newcomers |cffffffffguild.  Let's take care of that!")
 		else
-			DEFAULT_CHAT_FRAME:AddMessage("|cffffcc00Not a Newcomer!: |cffffffffYou are a member of the |cff00FF00" ..guildName.. " |cffffffffguild.  You're good to go!")
+			DEFAULT_CHAT_FRAME:AddMessage("|cffffcc00Not a Newcomer!: |cffffffffYou are currently a member of the |cff00FF00" ..guildName.. " |cffffffffguild.  You're good to go!")
 		end
 	else
 		DEFAULT_CHAT_FRAME:AddMessage("|cffffcc00Not a Newcomer!: |cffffffffHey!  You aren't in any guild at all!")
 	end
-frame:UnregisterEvent("CHAT_MSG_SYSTEM");
+frame:UnregisterEvent("UI_INFO_MESSAGE");
 end);
